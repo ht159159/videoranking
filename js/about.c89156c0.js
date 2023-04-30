8e0eb215eb88fc5f6757f3e1a241156db352652f
unknown <ht159159@gmail.com>
2023-04-30 15:59:46 +0800

    deploy

---
+++ js/about.c89156c0.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvideoranking"]=self["webpackChunkvideoranking"]||[]).push([[443],{7381:function(n,a,e){e.r(a),e.d(a,{default:function(){return f}});var r=e(3396);const u={class:"about"},i=(0,r._)("h1",null,"This is an about page",-1),t=[i];function s(n,a){return(0,r.wg)(),(0,r.iD)("div",u,t)}var c=e(89);const o={},d=(0,c.Z)(o,[["render",s]]);var f=d}}]);
+//# sourceMappingURL=about.c89156c0.js.map
